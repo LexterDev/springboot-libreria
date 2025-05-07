a33a2ef3c75015eba355003a0d9b538c22665d5d
Alex Galicia <gg14054@ues.edu.sv>
2025-05-06 19:19:00 -0600

    Initializing App - Corto 2 DAW

---
+++ src/test/java/com/springboot_libreria/springboot_libreria/SpringbootLibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_libreria.springboot_libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
